5c761b7d63c587a023f261e7184600f47f97be0c
vuflysocial <vufly.info@gmail.com>
2023-04-05 00:50:48 -0700

    yo

--- components/footer.tsx
+++ components/footer.tsx
@@ -24,6 +24,15 @@ const Footer = () => {
         >
           zodeyak.eth
         </a>
+        <a>
+          <iframe
+            src="https://ipfs.thirdwebcdn.com/ipfs/QmbAgC8YwY36n8H2kuvSWsRisxDZ15QZw3xGZyk9aDvcv7/erc1155.html?contract=0xdEEC826A258Df5290e1fc8f7190DFf2f31E7E616&chain=%7B%22name%22%3A%22Binance+Smart+Chain+Testnet%22%2C%22chain%22%3A%22BSC%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fbinance-testnet.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Binance+Chain+Native+Token%22%2C%22symbol%22%3A%22tBNB%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22bnbt%22%2C%22chainId%22%3A97%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22binance-testnet%22%7D&tokenId=0&theme=dark&primaryColor=green"
+            width="600px"
+            height="600px"
+            style={{ maxWidth: "100%" }}
+            frameBorder="0"
+          ></iframe>
+        </a>
       </h4>
       <p className="font-semibold">Melo was not hurt building this game.</p>
     </>
